7f842aa04c35135412e3c8c57e8dedcfa9587702
Callum Byrne <callumjbyrne97@gmail.com>
2021-09-25 00:24:10 +1000

    part1

---
+++ part1/part1/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Billy" />
+      <Hello name="Bob" />
+    </div>
+  )
+}
+
+export default App
