195414caddf8fec399d70b7ded5f95bec1834c34
Callum Byrne <callumjbyrne97@gmail.com>
2022-01-14 23:50:17 +1100

    bloglist step4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,12 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const likesArray = blogs.map(blog => blog.likes)
+    return likesArray.reduce((prev, cur) => prev + cur, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
