6abf3e4937711dd77265701b897b057a1841151b
Callum Byrne <callumjbyrne97@gmail.com>
2022-01-19 14:35:19 +1100

    bloglist step8

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -2,10 +2,10 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 // get all
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs.map(blog => blog.toJSON()))
-  })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+
+  response.json(blogs)
 })
 
 // get one
--- part4/bloglist/tests/bloglist_api.test.js
+++ part4/bloglist/tests/bloglist_api.test.js
@@ -4,7 +4,6 @@ const app = require('../app')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
-const { TestWatcher } = require('jest')
 
 const initialBlogs = [
     {
