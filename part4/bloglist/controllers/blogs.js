3dc45f733582af1066139108264f40446a200b07
Callum Byrne <callumjbyrne97@gmail.com>
2022-01-25 17:29:22 +1100

    bloglist step17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,12 +1,13 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 // using express-async-error npm package which is required on the app.js file
 // eliminates the need for try-catch blocks
 
 // get all
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
@@ -22,10 +23,24 @@ blogsRouter.get('/:id', async (request, response) => {
 
 // create
 blogsRouter.post('/', async (request, response, next) => {
-  const blog = new Blog(request.body)
+  const body = request.body
+
+  // const user = await User.findById(body.userId)
+  const allUsers = await User.find({})
+  const user = allUsers[0]
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
 
   const savedBlog = await blog.save()
-  response.json(savedBlog.toJSON())
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  response.json(savedBlog)
 })
 
 // delete
