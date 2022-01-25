c2415dba65b69eddbb2a753c48dfebfd13d43267
Callum Byrne <callumjbyrne97@gmail.com>
2022-01-25 15:16:27 +1100

    bloglist step16

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -5,6 +5,10 @@ const User = require('../models/user')
 usersRouter.post('/', async (request, response) => {
     const body = request.body
 
+    if (body.password.length < 3) {
+        return response.status(400).json({ error: 'password must be at least 3 charaters long' })
+    }
+
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
