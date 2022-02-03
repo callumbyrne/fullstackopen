d66d4a3df6b5369df3b97b1e7486671bfc0aebd8
Callum Byrne <callumjbyrne97@gmail.com>
2022-02-03 13:34:16 +1100

    bloglist-frontend step12 eslint

--- part5/bloglist-frontend/src/components/BlogForm.js
+++ part5/bloglist-frontend/src/components/BlogForm.js
@@ -1,59 +1,59 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
 
 const BlogForm = ({ createBlog }) => {
-    const [newTitle, setNewTitle] = useState('')
-    const [newAuthor, setNewAuthor] = useState('')
-    const [newUrl, setNewUrl] = useState('')
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newUrl, setNewUrl] = useState('')
 
-    // handlers
-    const handleTitleChange = (e) => {
-        setNewTitle(e.target.value)
-    }
+  // handlers
+  const handleTitleChange = (e) => {
+    setNewTitle(e.target.value)
+  }
 
-    const handleAuthorChange = (e) => {
-        setNewAuthor(e.target.value)
-    }
+  const handleAuthorChange = (e) => {
+    setNewAuthor(e.target.value)
+  }
 
-    const handleUrlChange = (e) => {
-        setNewUrl(e.target.value)
-    }
+  const handleUrlChange = (e) => {
+    setNewUrl(e.target.value)
+  }
 
-    const addBlog = (e) => {
-        e.preventDefault()
-        createBlog({
-            title: newTitle,
-            author: newAuthor,
-            url: newUrl
-        })
+  const addBlog = (e) => {
+    e.preventDefault()
+    createBlog({
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl
+    })
 
-        setNewTitle('')
-        setNewAuthor('')
-        setNewUrl('')
-    }
+    setNewTitle('')
+    setNewAuthor('')
+    setNewUrl('')
+  }
 
-    return (
-        <div>
-            <h2>Create new</h2>
-            <form onSubmit={addBlog}>
-                <div>title: <input
-                    value={newTitle}
-                    onChange={handleTitleChange}
-                />
-                </div>
-                <div>author: <input
-                    value={newAuthor}
-                    onChange={handleAuthorChange}
-                />
-                </div>
-                <div>url: <input
-                    value={newUrl}
-                    onChange={handleUrlChange}
-                />
-                </div>
-                <button type="submit">create</button>
-            </form>
+  return (
+    <div>
+      <h2>Create new</h2>
+      <form onSubmit={addBlog}>
+        <div>title: <input
+          value={newTitle}
+          onChange={handleTitleChange}
+        />
         </div>
-    )
+        <div>author: <input
+          value={newAuthor}
+          onChange={handleAuthorChange}
+        />
+        </div>
+        <div>url: <input
+          value={newUrl}
+          onChange={handleUrlChange}
+        />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
 }
 
 export default BlogForm
