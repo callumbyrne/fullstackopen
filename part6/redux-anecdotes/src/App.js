065bb0106db9d2d367acbb22e68a2476e777f3b3
Callum Byrne <callumjbyrne97@gmail.com>
2022-02-09 15:35:50 +1100

    anecdotes redux step1-6

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NewAnecdote from './components/AnecdoteForm'
+import Anecdotes from './components/AnecdoteList'
+
+const App = () => {
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <Anecdotes />
+      <NewAnecdote />
+    </div>
+  )
+}
+
+export default App
