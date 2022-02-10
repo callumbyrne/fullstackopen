b00036c0386bd5b97454f07920d6f69d25126417
Callum Byrne <callumjbyrne97@gmail.com>
2022-02-10 21:36:51 +1100

    anecdotes backend step1-2

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NewAnecdote from './components/AnecdoteForm'
 import Anecdotes from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import { useDispatch } from 'react-redux'
+import anecdoteService from './services/anecdotes'
+import { initalizeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+      .getAll().then(anecdote => dispatch(initalizeAnecdotes(anecdote)))
+  }, [dispatch])
+
   return (
     <div>
       <h2>Anecdotes</h2>
