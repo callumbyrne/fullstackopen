b4337a1a8ba093c069fa3763812ac0e8465ab257
Callum Byrne <callumjbyrne97@gmail.com>
2022-03-02 12:10:50 +1100

    8.9 books view

--- part8/graphql-library-frontend/src/components/Authors.js
+++ part8/graphql-library-frontend/src/components/Authors.js
@@ -1,48 +1,48 @@
-import { gql, useQuery } from "@apollo/client";
+import { gql, useQuery } from '@apollo/client'
 
 const ALL_AUTHORS = gql`
-    query {
-        allAuthors {
-            name
-            born
-            bookCount
-            id
-        }
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+      id
     }
-`;
+  }
+`
 
 const Authors = (props) => {
-    const authors = useQuery(ALL_AUTHORS);
+  const result = useQuery(ALL_AUTHORS)
 
-    if (!props.show) {
-        return null;
-    }
+  if (!props.show) {
+    return null
+  }
 
-    if (authors.loading) {
-        return <div>loading...</div>;
-    }
+  if (result.loading) {
+    return <div>loading...</div>
+  }
 
-    return (
-        <div>
-            <h2>authors</h2>
-            <table>
-                <tbody>
-                    <tr>
-                        <th></th>
-                        <th>born</th>
-                        <th>books</th>
-                    </tr>
-                    {authors.data.allAuthors.map((a) => (
-                        <tr key={a.name}>
-                            <td>{a.name}</td>
-                            <td>{a.born}</td>
-                            <td>{a.bookCount}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
-};
+  return (
+    <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>born</th>
+            <th>books</th>
+          </tr>
+          {result.data.allAuthors.map((a) => (
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.bookCount}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
 
-export default Authors;
+export default Authors
